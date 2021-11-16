3999ebcd7d0039b1ec7c0cd4d50209a1f96feab4
Junrong <junrongh94@outlook.com>
2021-11-16 22:31:12 +0800

    init loader, verify czi file

--- czi-js/src/CZIReader.ts
+++ czi-js/src/CZIReader.ts
@@ -1,5 +1,20 @@
 export default class CZIReader {
+    buffer: ArrayBuffer = null;
     constructor() {}
+
+    public async load(src: string) {
+        const response = await fetch(src);
+        const ab = await response.arrayBuffer();
+        const dec = new TextDecoder()
+        const header = dec.decode(ab.slice(0, 10));
+        if (header === 'ZISRAWFILE') {
+            this.buffer = ab;
+            return ab;
+        } else {
+            console.error('Error: not a czi file');
+            return;
+        }
+    }
 
     public test() {
         console.log('test');
--- czi-js/src/index.ts
+++ czi-js/src/index.ts
@@ -5,4 +5,7 @@ const canvas = document.getElementById('mycanvas');
 const reader = new CZIReader();
 
 reader.test();
+reader.load('./dist/public/EPSC-em1_CK18_10x.czi').then(res => {
+    console.log(res);
+})
 window['reader'] = reader;
